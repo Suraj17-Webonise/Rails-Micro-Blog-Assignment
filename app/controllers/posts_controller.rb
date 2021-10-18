827fd2dac43859ff718e48a36728056e63c83c31
Suraj17-Webonise <suraj.mathpati@weboniselab.com>
2021-10-18 16:00:35 +0530

    Publish/Unpublish Posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,68 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  
+  before_action :find_id, only: [:edit, :update, :show, :destory, :unpublish, :publish]
+
+  def show
+    #@post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def edit
+    #@post = Post.find(params[:id])
+  end
+
+  def update
+    #@post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def publish
+    #@post = Post.find(params[:id])
+    @post.update(ispublished: true)
+    redirect_to root_path
+  end
+
+  def unpublish
+    #@post = Post.find(params[:id])
+    @post.update(ispublished: false)
+    redirect_to root_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def find_id
+    @post = Post.find(params[:id])
+  end
+end
